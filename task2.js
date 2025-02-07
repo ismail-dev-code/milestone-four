a1921cd38ff3fa66d3c75b6df556f1cdd7d8b4f7
ismail-dev-code <ismail.online0385678@gmail.com>
2025-02-07 11:37:34 +0600

    task2 updated

--- task2.js
+++ task2.js
@@ -1,7 +1,5 @@
 let destination = ["coxs bazar", "sajek", "sylhet"];
-destination.push("noakhali")
-destination.push("ctg")
-destination.push("dhaka")
-// console.log(destination);
+destination.push("noakhali");
+destination.push("ctg", "dhaka");
 destination.pop()
 console.log(destination);
