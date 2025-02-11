f7555e60770002ddc6cf832fa155a4d829a96578
ismail-dev-code <ismail.online0385678@gmail.com>
2025-02-11 10:44:23 +0600

    task added

---
+++ task1.js
@@ -0,0 +1,5 @@
+const colors = ["red", "yellow", "green", "brown"];
+
+for (let i = colors.length - 1; i >= 0; i--) {
+  console.log(colors[i]);
+}
---
+++ task3.js
@@ -0,0 +1,6 @@
+let result = '';
+const numbers = ["tom", "tin", "tik", "tim"];
+for(const num of numbers){
+    result = result + num;
+}
+console.log(result);
