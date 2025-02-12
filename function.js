bd1162b04122679b577179843b9f6a2ab840ad07
ismail-dev-code <ismail.online0385678@gmail.com>
2025-02-13 00:31:34 +0600

    initialized done

---
+++ function.js
@@ -0,0 +1,14 @@
+const money = 400;
+function myWallet() {
+  console.log("pera nai ghumai thako");
+  console.log("ami kintu jani na eti");
+  console.log("interesting bepar ta");
+  console.log("ar ki likhmu re ba");
+}
+myWallet();
+function orangePrice() {
+  console.log("edesher manush ektu beshi pakna");
+  console.log("pakna manush valo na re");
+  console.log("kita koitam re ba");
+}
+orangePrice();
---
+++ perameter.js
@@ -0,0 +1,11 @@
+function myAmount(num1, num2) {
+  const result = num1 * num2;
+  console.log(result);
+}
+myAmount(3, 100);
+
+function mySum(sum1, sum2) {
+  const result = sum1 + sum2;
+  console.log(result);
+}
+mySum(100, 5);
